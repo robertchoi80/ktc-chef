8a0ed0805c9da145230ec89d7818875fe4d8bc99
Wil Reichert <wil.reichert@kt.com>
2014-02-26 09:07:56 +0900

    ensure cron service is running

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,11 @@
   include_recipe recipe
 end
 
+# ensure cron is running
+service 'cron' do
+  action :start
+end
+
 node['kt']['report_handlers'].each do |h|
   include_recipe "ktc-chef::_chef_handler_#{h}"
 end
