2e960ef5e27ec82de3f73bbc5d4886206ff58b8f
Robert <taeil.choi@kt.com>
2014-03-26 11:27:31 +0900

    move resources to run_time phase & add build-essential

--- recipes/_chef_handler_sensu.rb
+++ recipes/_chef_handler_sensu.rb
@@ -5,12 +5,6 @@
 include_recipe 'chef_handler'
 include_recipe 'services'
 
-ep = Services::Endpoint.new 'sensu-rabbitmq'
-ep.load
-
-Chef::Log.info("rabbit host: #{ep.ip}")
-Chef::Log.info("rabbit port: #{ep.port}")
-
 gem_packages = {
   'oj' => '2.0.9',
   'amq-protocol' => '1.2.0',
@@ -18,12 +12,22 @@ gem_packages = {
   'amqp' => '1.0.0'
 }
 
+ep = Services::Endpoint.new 'sensu-rabbitmq'
+ep.load
+
+Chef::Log.info("rabbit host: #{ep.ip}")
+Chef::Log.info("rabbit port: #{ep.port}")
+
+# This is needed for compiling Oj native extension.
+# Would be used until we have pre-compiled Oj package.
+package "build-essential"
+
 gem_packages.each do |pkg, ver|
-  gem_package pkg do
-    gem_binary '/opt/chef/embedded/bin/gem'
+  chef_gem pkg do
     version ver
     action :nothing
-  end.run_action(:install)
+    subscribes :install, "package[build-essential]", :immediately
+  end
 end
 
 args = []
@@ -44,14 +48,14 @@ end
 cookbook_file "#{node['chef_handler']['handler_path']}/_rabbitmq.rb" do
   source 'rabbitmq.rb'
   mode 0640
-  action :nothing
-end.run_action(:create)
+  action :create
+end
 
 cookbook_file "#{node['chef_handler']['handler_path']}/sensu_handler.rb" do
   source 'sensu_handler.rb'
   mode 0640
-  action :nothing
-end.run_action(:create)
+  action :create
+end
 
 chef_handler 'Chef::Handler::Sensu::ReportSensu' do
   source "#{node['chef_handler']['handler_path']}/sensu_handler.rb"
