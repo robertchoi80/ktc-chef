6b39f39cfdbb83d7dd1a99bcf1fa7f6122a760ba
ktc-jenkins <jenkins.ktcloudware@gmail.com>
2014-04-01 09:43:58 +0900

    auto_version to 0.0.6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'wil.reichert@ktcloudware.com'
 license          'All rights reserved'
 description      'configure chef client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.5'
+version '0.0.6'
 
 %w(centos ubuntu).each do |os|
   supports os
