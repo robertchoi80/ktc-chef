01974083967818b0913d2c3b2fd8bde9240ca441
Robert <taeil.choi@kt.com>
2014-04-04 19:34:19 +0900

    remove redundant attrs

--- attributes/chef_client.rb
+++ attributes/chef_client.rb
@@ -10,8 +10,3 @@ default[:chef_client][:cron][:hour] = '*'
 default[:chef_client][:cron][:log_file] = '/var/log/chef/client_cron.log'
 default[:chef_client][:cron][:minute] = '*/15'
 default[:chef_client][:log_file] = '/var/log/chef/client.log'
-
-# This is for calculating sensu_handler report interval
-# Refer to the above node[:chef_client][:cron] and derive from it.
-# Eg) If the 'minute' is '*/15', then [:cron][:interval] should be 15.
-default[:chef_client][:cron][:interval] = 15
