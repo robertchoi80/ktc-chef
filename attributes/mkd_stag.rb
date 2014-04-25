1f191b4c7808e1993c65644bf55d52839ae543b0
Robert <taeil.choi@kt.com>
2014-04-25 11:36:24 +0900

    add mkd_stag attr

---
+++ attributes/mkd_stag.rb
@@ -0,0 +1,18 @@
+return unless chef_environment.eql?('mkd_stag')
+
+default['kt']['report_handlers'] = %w(
+  json
+  graphite
+  hipchat
+  sensu
+)
+
+default['kt']['report_interval'] = 2
+
+# parameters to the hipchat report handler, order matters.
+default['kt']['hipchat'] = {
+  api_token: '598de357f0ae8d7f8f3556e0cf8347',
+  room_name: 'chef_alerts',
+  notify_users: false,
+  report_success: false
+}
